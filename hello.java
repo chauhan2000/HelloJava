5e1adbebe5ccfba569dbf90ac1ff2a314bcc2d2d
Rachit Chauhan <36044440+chauhan2000@users.noreply.github.com>
2019-09-17 00:45:24 +0530

    Hello Java first program of java

---
+++ hello.java
@@ -0,0 +1,19 @@
+class hello //creates class 
+{
+    void fun() // creates method
+    {
+       System.out.println("Hello Java"); // function is used to print the statement
+    }
+}
+class display
+{
+public static void main(String[] args) //main method
+{
+hello obj = new hello(); // creating object
+obj.fun(); // calling function by object
+}
+}
+
+
+
+//run this command by removing the comments
